12a1ad448a4cd75f3033290129144358ea7dac3d
Fran Vidicek <franvidicek@gmail.com>
2024-06-29 07:18:16 +0200

    add landing page

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import TitleSection from "@/components/landing-page/title-section";
+import Banner from "../../../public/BannerImage.png";
+
+const HomePage = () => {
+  return (
+    <>
+      <section>
+        <div
+          className="
+          overflow-hidden
+          px-4
+          sm:px-6
+          mt-10
+          sm:flex
+          sm:flex-col
+          gap-4
+          md:justify-center
+          md:items-center
+          "
+        >
+          <TitleSection
+            pill=" Your Workspace, Perfected"
+            title="All-In-One Collaboration and Productivity Platform"
+          />
+          <div className="bg-white p-[2px] mt-xl rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
+            <Button
+              variant="btn-secondary"
+              className=" w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background
+          "
+            >
+              Get Cypress Free
+            </Button>
+          </div>
+          <div
+            className="md:mt-[-90px]
+          sm:w-full
+          w-[750px]
+          flex
+
+          justify-center
+          items-center
+          mt-[-40px]
+          relative
+          sm:ml-0
+          ml-[-50px]"
+          >
+            <Image src={Banner} alt="Application Banner" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
